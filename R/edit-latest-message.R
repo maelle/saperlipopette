74d7bd59837fd4a139d1feb270477cf5d9d4d368
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-12-18 10:14:02 +0100

    add another function

---
+++ R/edit-latest-message.R
@@ -0,0 +1,56 @@
+#' "Oh shit, I need to change the message on my last commit!"
+#'
+#' @description
+#' To go with <https://ohshitgit.com/#change-last-commit-message>
+#'
+#'
+#' @param parent_path Path where to create the exercise repo
+#'
+#' @return The path
+#' @export
+#'
+#' @examplesIf interactive()
+#' parent_path <- withr::local_tempdir()
+#' path <- exo_latest_message(parent_path = parent_path)
+#' fs::dir_tree(path)
+#' gert::git_log(repo = path)
+exo_latest_message <- function(parent_path) {
+
+  path <- file.path(parent_path, "latest-message")
+
+  withr::local_options(usethis.quiet = TRUE)
+
+  fs::dir_create(path)
+  original_dir <- getwd()
+
+  withr::local_dir(path)
+  gert::git_init()
+
+  file.copy(
+    system.file("exo_latest_message-Rprofile.R", package = "saperlipopette"),
+    ".Rprofile"
+  )
+
+  usethis::create_project(path = getwd())
+  # Ignore Rproj that might otherwise get edited when we open the project
+  rproj <- fs::dir_ls(glob = "*.Rproj")
+  usethis::local_project(getwd(), force = TRUE)
+  usethis::use_git_ignore(rproj)
+  usethis::use_git_ignore(".Rprofile")
+  gert::git_add("*")
+  git_commit("First commit")
+
+  fs::file_create("bla")
+  brio::write_lines(
+    text = c("thing 1", "thing 2"),
+    path = "bla"
+  )
+  gert::git_add("bla")
+  git_commit("add a new documennnt")
+
+  usethis::local_project(original_dir, force = TRUE)
+
+  cli::cli_alert_info("Follow along in {path}!")
+
+  return(path)
+}
---
+++ inst/exo_latest_message-Rprofile.R
@@ -0,0 +1,15 @@
+if (file.exists("~/.Rprofile")) {
+  base::sys.source("~/.Rprofile", envir = environment())
+}
+
+cli::cli_alert_danger('"Oh shit, I need to change the message on my last commit!')
+cli::cli_alert_danger("I made a typo!")
+cli::cli_alert_info("See {.url https://ohshitgit.com/#change-last-commit-message}")
+cli::cli_li(
+  items = c(
+    "Examine Git history.",
+    "Notice 'documennnt' should be 'document'! A typo!",
+    "git commit --amend",
+    "Examine Git history."
+  )
+)
